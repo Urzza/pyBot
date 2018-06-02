53e93bc98a4af7982481e2c1f51f0808e1ecc01d
mc.asdfgf <mc.asdfgf@protonmail.com>
2018-06-02 22:12:19 +0700

    WOO :D

--- pybot.py
+++ pybot.py
@@ -1,17 +1,61 @@
 import config
+import validators as vl
+import re
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 token = config.token
-
+admin_id = 189766950
 updater = Updater(token = token)
 dispatcher = updater.dispatcher
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Hello! I'm a LIVE!")
+    bot.send_message(chat_id=update.message.chat_id, text="""
+    Hello!
+    This is a Tags Bot. It to service @lifeONet channel.
+    Usage:
+    /start or /help - this text
+    /tags - view list all tags
+    """)
 
+def tags(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="tags")
+    
+def tagUpdate(bot, update):
+    chat_id = update.message.chat_id
+    if isAdmin(chat_id):
+        msg_text = update.message.text
+        prep_query = re.findall('(#\w+)\s(https?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),])+)', msg_text)
 
-start_handler = CommandHandler('start', start)
+        if not prep_query :
+            
+            info = """
+            Update tag info or added tag into DB.
+            Usage: /tagupdate #nameTag http://link.to/post?or=another&url=1
+            """
+        
+            bot.send_message(chat_id, text=info)
+        else:
+            tag = prep_query[0][0]
+            url = prep_query[0][1]
+            # TODO: save to DB
+            
+
+    else:
+        bot.send_message(chat_id, text="ACCESS DENIED")
+                            
+def isAdmin(chat_id):
+    if chat_id == admin_id:
+        return True
+    else:
+        return False
+
+start_handler = CommandHandler(['start', 'help'], start)
+tagUpdate_handler = CommandHandler('tagupdate', tagUpdate)
+tags_handler = CommandHandler('tags', tags)
+dispatcher.add_handler(tags_handler)
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(tagUpdate_handler)
+
 # dispatcher.add_handler(MessageHandler(Filters.text, echo, channel_post_updates = True))
 
 updater.start_polling()
